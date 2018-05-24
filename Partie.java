9170cce1d9441f4dfd64f45b0ce7a57bcb92907a
lionelfayolle <33628543+lionelfayolle@users.noreply.github.com>
2018-05-24 09:35:08 +0200

    Update Partie.java

--- Partie.java
+++ Partie.java
@@ -7,6 +7,7 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Partie {
 	public int nbre_joueur;
 	public int nbre_territoire;
+	
 
 	public Partie(int ter, int jou) {
 		this.nbre_territoire = ter;
@@ -31,45 +32,41 @@ public class Partie {
 
 	public void afficher_map() {
 		StdDraw.clear(StdDraw.WHITE);
-		StdDraw.picture(50.0, 50.0, "map.png");// taille de la map 640*480
+		StdDraw.picture(50.0,50.0,"IMAGE/parchemin_2.png");
+		StdDraw.picture(50.0,50.0,"IMAGE/map.png");
 		StdDraw.setPenColor(StdDraw.GRAY);
 		StdDraw.filledRectangle(110, 100, 6, 4);// bouton mission
 		StdDraw.filledRectangle(110, 90, 6, 4);// bouton finir son tour
 		StdDraw.filledRectangle(110, 80, 6, 4);// bouton attaquer
-		StdDraw.filledRectangle(110, 70, 6, 4);// bouton se déplacer
-		StdDraw.setPenColor(StdDraw.BLACK);// couleur de l'écriture
-		StdDraw.line(100, 0, 100, 110);
-		StdDraw.text(110, 100, "Mission");// texte
-		StdDraw.text(110, 90, "Finir");// texte
-		StdDraw.text(110, 80, "Attaque");// texte
-		StdDraw.text(110, 70, "Mouvement");// texte
+		StdDraw.filledRectangle(110, 70, 6, 4);// bouton se dÃ©placer
+		StdDraw.setPenColor(StdDraw.BLACK);// couleur de l'Ã©criture
+		StdDraw.text(110, 100, "mission");// texte
+		StdDraw.text(110, 90, "finir");// texte
+		StdDraw.text(110, 80, "attaque");// texte
+		StdDraw.text(110, 70, "mouvement");// texte
 	}
-
-	public void afficher_territoire() {
-		StdDraw.picture(50.0, 50.0, "2.png");
-	}
-
-	public static ArrayList<territoire> randomAvecExclusion(int min, int max) {
-		ArrayList resultat = new ArrayList<territoire>();
-		Random rand = new Random();
-		while (resultat.size() != 42) {
-			int s = 0;
-			territoire res2 = new territoire(rand.nextInt(max - min) + min, 1);
-			for (int i = 0; i < resultat.size(); i++) {
-				if (res2.getNumero() == ((territoire) resultat.get(i)).getNumero()) {
+	
+	public static ArrayList<territoire> randomAvecExclusion(int min, int max){ 
+		ArrayList resultat = new ArrayList<territoire>(); 
+		Random rand = new Random(); 
+		while (resultat.size()!=42){ 
+			int s=0;
+			territoire res2 = new territoire (rand.nextInt(max - min)+ min,1);
+			for (int i=0;i<resultat.size();i++) {
+				if (res2.getNumero()==((territoire)resultat.get(i)).getNumero()) {
 					s++;
 				}
 			}
-			if (s == 0) {
+			if (s==0) {
 				resultat.add(res2);
 			}
-		}
-		return resultat;
+		} 
+		return resultat; 
 	}
-
+	
 	public ArrayList ajouter_teritoire(Joueur j, List<territoire> l1) {
-		for (int i = 0; i < l1.size(); i++) {
-			territoire y = (territoire) l1.get(i);
+		for (int i=0;i<l1.size();i++) {
+			territoire y=(territoire) l1.get(i);
 			j.getList_ter().add(y);
 		}
 		return j.list_ter;
