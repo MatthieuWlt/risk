bf5651a9b67e9aa67e519b346288165d56d6c517
MatthieuWlt <willot.matthieu@gmail.com>
2018-05-25 16:32:24 +0200

    Update Menu.java
    Correction bug

--- risk-master.zip_expanded/risk-master/Menu.java
+++ risk-master.zip_expanded/risk-master/Menu.java
@@ -10,10 +10,11 @@ import javax.swing.*;
 public class Menu {
 
 	public static void main(String[] args) throws InterruptedException {
+		
+		/* Initialisation de la taille de la fenêtre et affichage du fond d'into */
 		StdDraw.setCanvasSize(1229, 576);
-		StdDraw.setXscale(0.0, 120);/* INITIALISATION DE LA TAILLE DE LA FENETRE */
+		StdDraw.setXscale(0.0, 120);
 		StdDraw.setYscale(0.0, 110);
-
 		afficherFond(60.0, 55.0, "Image/fond_intro.JPG");
 
 		// Définition d'une police et de sa taille
@@ -22,21 +23,23 @@ public class Menu {
 		currentFont = currentFont.deriveFont(size);
 		StdDraw.setFont(currentFont);
 
-		/* INITIALISATION DES CHOIX */
-		int choix1 = 0;// cliquer pour atteindre le menu
-		int choix2 = 1;// cliquer pour sélectionner le nombre de joueur
-		int choix3 = 0;
+		/* Initialisation des choix */
+		int choix1 = 0; // Pour atteindre le menu
+		int choix2 = 1;
+		int choix3 = 0; // Pour le choix du nombre de joueurs et l'affichage de la carte
 		int choix4 = 1;
-		int choix5 = 0;
+		int choix5 = 0; // Choix du personnage
 		String choixJ1 = "";
 		String choixJ2 = "";
 		Random rand = new Random();
 
+		
 		while (choix1 != 1) {
-			if (StdDraw.isMousePressed()) { // si l'utilisateur clique, alors on affiche
+			if (StdDraw.isMousePressed()) {
 				afficherFond(60.0, 55.0, "Image/back2.png");
 				choix2 = 0;
 
+				/* Affichage d'une image en fonction du nombre de joueurs */	
 				while (choix2 != 1) {
 					if (StdDraw.mousePressed()) {
 						double x = StdDraw.mouseX();
@@ -66,14 +69,12 @@ public class Menu {
 					} else {
 					}
 				}
-				// StdDraw.pause(500);
-			} /* l'utilisateur ne clique pas, le menu ne s'affiche pas CHOIX 1 */
+			}
+
+
+/* ----------------------------- Initialisation pour 2 JOUEURS ----------------------------- */
 
 			/*
-			 * ----------------------------- Initialisation pour 2 JOUEURS
-			 * -----------------------------
-			 */
-
 			while (choix3 == 2) {
 
 				if (StdDraw.isMousePressed()) {
@@ -124,184 +125,208 @@ public class Menu {
 					}
 				}
 				break;
-			}
+			}*/
 
-			// 2 JOUEURS
-			while (choix5 == 2) {
+			while (choix3 == 2) {
 				if (StdDraw.mousePressed()) {
 					int armee = 40;
 					int choix41 = 1;
-					Partie p = new Partie(42, choix3); // création de la carte
-					p.afficher_map(); // affichage de la carte
-					// création joueurs
+					
+					// Création et affichage de la carte
+					Partie p = new Partie(42, choix3); 
+					p.afficher_map(); 
+					
+					// Création des joueurs
 					Joueur j1 = new Joueur(0, 1, StdDraw.BLUE, "bleu", null);
 					Joueur j2 = new Joueur(0, 2, StdDraw.GREEN, "vert", null);
 
-					// attribution de la mission au joueur 1
+					// Attribution de la mission au joueur 1
 					int choixmissionj1 = 0;
 					while (choixmissionj1 == 0) {
-						if (StdDraw.mousePressed()) {
-							double x4 = StdDraw.mouseX();// on recupere la coordonnée de x cliqué
-							double y4 = StdDraw.mouseY();// on recupere la coordonnée de y cliqué
-							if (x4 < 116 && x4 > 104 && y4 < 104 && y4 > 96) {// si elles sont sur le bouton
-								int missionj1 = rand.nextInt(2 - 1) + 1;// on choisit une mission aléatoire
-								j1.attribution_mission(missionj1, 2, j1);// on attribut une mission au joueur 1
-								j1.setMission(missionj1);// on attribut la mission 1 au joueur 1
-								choixmissionj1 = 1;// on termine la boucle while
+						if (StdDraw.isMousePressed()) {
+							
+							// On récupère les coordonnées x et y du clic
+							double x4 = StdDraw.mouseX();
+							double y4 = StdDraw.mouseY();
+							
+							// Si le clic est sur le bouton
+							if (x4 < 116 && x4 > 104 && y4 < 104 && y4 > 96) { 
+								int missionj1 = rand.nextInt(2 - 1) + 1; // On choisit une mission aléatoire
+								j1.attribution_mission(missionj1, 2, j1); // On attribut une mission au joueur 1
+								j1.setMission(missionj1); // On attribut la mission 1 au joueur 1
+								choixmissionj1 = 1; // On termine la boucle while
 							} else {
-								choixmissionj1 = 0;// on redemande au joueur de choisir sa mission
-								StdDraw.text(60, 105, " Joueur 1, découvrez votre mission");// affichage
+								choixmissionj1 = 0; // On redemande au joueur de choisir sa mission
+								StdDraw.text(60, 105, " Joueur 1, découvrez votre mission");
 							}
 						} else {
-							choixmissionj1 = 0;// on demande au joueur de choisir sa mission
-							StdDraw.text(60, 105, "Joueur 1, découvrez votre mission");// affichage
+							choixmissionj1 = 0;// On demande au joueur de choisir sa mission
+							StdDraw.text(60, 105, "Joueur 1, découvrez votre mission");
 						}
 					}
 
-					StdDraw.clear();// on supprime le message de mission
-					p.afficher_map(); // on ré-affiche la carte
+					// On supprime le message de mission et on ré-affiche la carte
+					StdDraw.clear();
+					p.afficher_map(); 
 
-					// attribution de la mission au joueur 2
+					/* Attribution de la mission au joueur 2 */
 					int choixmissionj2 = 0;
 
 					while (choixmissionj2 == 0) {
-						if (StdDraw.mousePressed()) {
-							double x5 = StdDraw.mouseX();// on recupere les coordonnées de x et y cliqués
+						if (StdDraw.isMousePressed()) {
+							
+							double x5 = StdDraw.mouseX();
 							double y5 = StdDraw.mouseY();
-							if (x5 < 116 && x5 > 104 && y5 < 104 && y5 > 96) {// si elles sont sur le bouton
+							
+							if (x5 < 116 && x5 > 104 && y5 < 104 && y5 > 96) {
 								int missionj2 = rand.nextInt(8 - 1) + 1;
 								j2.setMission(missionj2);
-								j2.attribution_mission(missionj2, 2, j2);// on attribut une mission au
-																			// joueur 1
+								j2.attribution_mission(missionj2, 2, j2);
 								choixmissionj2 = 1;
 							} else {
-								choixmissionj2 = 0;// on redemande au joueur de choisir sa mission
+								choixmissionj2 = 0;
 								StdDraw.text(60, 105, " Joueur 2, découvrez votre mission");
 							}
 						} else {
-							choixmissionj2 = 0;// on demande au joueur de choisir sa mission
+							choixmissionj2 = 0;
 							StdDraw.text(60, 105, "Joueur 2, découvrez votre mission");
 						}
 					}
 
-					StdDraw.clear();// on supprime le message de mission
-					p.afficher_map(); // on ré-affiche la carte
+					// On supprime le message de mission et on ré-affiche la carte
+					StdDraw.clear();
+					p.afficher_map(); 
 
-					j1.verification_mission(j1.getMission(), j1);// on verifie que le joueur n'a pas gagné
-					j2.verification_mission(j2.getMission(), j2);// on vérifie que le joueur n'a pass gagné
+					// On vérifie qu'aucun joueur n'a gagné
+					j1.verification_mission(j1.getMission(), j1);
+					j2.verification_mission(j2.getMission(), j2);
 
-					// liste aléatoire des territoires
+					// Liste aléatoire des territoires
 					ArrayList l = new ArrayList<territoire>();
 					l = p.randomAvecExclusion(1, 43);
-					// découpage de la liste selon le nombre de joueurs
+					
+					// Découpage de la liste selon le nombre de joueurs
 					List<territoire> l1 = l.subList(0, 21);
 					List<territoire> l2 = l.subList(21, 42);
-					// attribuer les territoires aux joueurs
+					
+					// Attribution des territoires aux joueurs
 					p.ajouter_teritoire(j1, l1);
 					p.ajouter_teritoire(j2, l2);
-					// affichage des territoires et armées des joueurs
+					
+					// Affichage des territoires et armées des joueurs
 					j1.territoire_joueur();
-					j2.territoire_joueur();
-
+					j2.territoire_joueur();;
+					
 					j1.affiche();
-
 					j2.affiche();
-
+					
 					placementArmeeRestantes(j1, j2, choix41);
 
 					for (int j = 0; j < 10; j++) { // nombre de tour arbitraire
-						System.out.println("joueur" + 1 + " place");
-						j1.initialisation(choix41, j1, 3); // placement des armées du joueur 1
-						System.out.println("le joueur1 attaque");
-						j1.phase_attaque(1, j1, j2); // phase attaque du joueur 1
-						System.out.println("le joueur1 déplace");
-						j1.deplacement(1, j1); // phase attaque du joueur 1
+						
+						// Affichage des textes pour un joueur
+						System.out.println("Joueur" + 1 + " place");
+						j1.initialisation(choix41, j1, 3); // Placement des armées du joueur 1
+						System.out.println("Le joueur1 attaque");
+						j1.phase_attaque(1, j1, j2); // Phase attaque du joueur 1
+						System.out.println("Le joueur1 déplace");
+						j1.deplacement(1, j1); //Phase attaque du joueur 1
 
-						System.out.println("joueur" + 2 + " place");
-						j2.initialisation(choix41, j2, 3); // placement des armées du joueur 2
-						System.out.println("le joueur2 attaque");
-						j2.phase_attaque(1, j2, j1); // phase attaque du joueur 2
-						System.out.println("le joueur2 déplace");
-						j1.deplacement(1, j2); // phase attaque du joueur 2
+						System.out.println("Joueur" + 2 + " place");
+						j2.initialisation(choix41, j2, 3);
+						System.out.println("Le joueur2 attaque");
+						j2.phase_attaque(1, j2, j1); 
+						System.out.println("Le joueur2 déplace");
+						j1.deplacement(1, j2); 
 					}
-
 					break;
 				}
 			}
 
-			// 3 JOUEURS
-			while (choix5 == 3) {
+/* ----------------------------- Initialisation pour 3 JOUEURS ----------------------------- */
+
+			while (choix3 == 3) {
 				if (StdDraw.mousePressed()) {
 					int armee = 35;
-					Partie p = new Partie(42, choix3); // création de la carte
-					p.afficher_map(); // affichage de la carte
-					// création joueurs
+
+					Partie p = new Partie(42, choix3); 
+					p.afficher_map(); 
+
 					Joueur j1 = new Joueur(0, 1, StdDraw.BLUE, "bleu", null);
 					Joueur j2 = new Joueur(0, 2, StdDraw.GREEN, "vert", null);
 					Joueur j3 = new Joueur(0, 3, StdDraw.RED, "rouge", null);
-					// liste aléatoire des territoires
+
 					ArrayList l = new ArrayList<territoire>();
 					l = p.randomAvecExclusion(1, 43);
-					// découpage de la liste selon le nombre de joueurs
+					
 					List<territoire> l1 = l.subList(0, 14);
 					List<territoire> l2 = l.subList(14, 28);
 					List<territoire> l3 = l.subList(28, 42);
-					// attribuer les territoires aux joueurs
+					
 					p.ajouter_teritoire(j1, l1);
 					p.ajouter_teritoire(j2, l2);
 					p.ajouter_teritoire(j3, l3);
-					// affichage des territoires des joueurs
+					
+					
 					j1.territoire_joueur();
 					j1.armee_joueur(armee);
+					j1.affiche();
+					
 					j2.territoire_joueur();
 					j2.armee_joueur(armee);
+					j2.affiche();
+					
 					j3.territoire_joueur();
 					j3.armee_joueur(armee);
-					j1.affiche();
-					j2.affiche();
 					j3.affiche();
 					break;
 				}
 
 			}
 
-			// 4 JOUEURS
-			while (choix5 == 4) {
+/* ----------------------------- Initialisation pour 4 JOUEURS ----------------------------- */
+			
+			while (choix3 == 4) {
 				if (StdDraw.mousePressed()) {
 					int armee = 30;
-					Partie p = new Partie(42, choix3); // création de la carte
-					p.afficher_map(); // affichage de la carte
-					// création joueurs
+					
+					Partie p = new Partie(42, choix3); 
+					p.afficher_map(); 
+					
 					Joueur j1 = new Joueur(0, 1, StdDraw.BLUE, "bleu", null);
 					Joueur j2 = new Joueur(0, 2, StdDraw.GREEN, "vert", null);
 					Joueur j3 = new Joueur(0, 3, StdDraw.RED, "rouge", null);
 					Joueur j4 = new Joueur(0, 4, StdDraw.ORANGE, "orange", null);
-					// liste aléatoire des territoires
+					
 					ArrayList l = new ArrayList<Integer>();
 					l = p.randomAvecExclusion(1, 43);
-					// découpage de la liste selon le nombre de joueurs
+
 					List l1 = l.subList(0, 10);
 					List l2 = l.subList(10, 21);
 					List l3 = l.subList(21, 31);
 					List l4 = l.subList(31, 42);
-					// attribuer les territoires aux joueurs
+
 					p.ajouter_teritoire(j1, l1);
 					p.ajouter_teritoire(j2, l2);
 					p.ajouter_teritoire(j3, l3);
 					p.ajouter_teritoire(j4, l4);
-					// affichage des territoires des joueurs
+
 					j1.territoire_joueur();
 					j1.armee_joueur(armee);
+					j1.affiche();
+					
 					j2.territoire_joueur();
 					j2.armee_joueur(armee);
+					j2.affiche();
+					
 					j3.territoire_joueur();
 					j3.armee_joueur(armee);
+					j3.affiche();
+					
 					j4.territoire_joueur();
 					j4.armee_joueur(armee);
-					j1.affiche();
-					j2.affiche();
-					j3.affiche();
 					j4.affiche();
+					
 					break;
 				}
 			}
@@ -320,13 +345,13 @@ public class Menu {
 		StdDraw.picture(x, y, fichier);
 		StdDraw.pause(500);
 	}
-
-	// Fonction permettant d'afficher le nombre d'armées qu'il reste à placer en
-	// console
-	public static void placementArmeeRestantes(Joueur j1, Joueur j2, int choix4) {
-		System.out.println("Joueur " + 1 + " place");
-		j1.initialisation(choix4, j1, 5); // placement des armées du joueur 1
-		System.out.println("joueur " + 2 + " place");
-		j2.initialisation(choix4, j2, 5); // placement des armées du joueur 2
-	}
+	
+	// Fonction permettant d'afficher le nombre d'armées qu'il reste à placer en console
+		public static void placementArmeeRestantes(Joueur j1, Joueur j2, int choix4) {
+			System.out.println("Joueur " + 1 + " place");
+			j1.initialisation(choix4, j1, 5); // placement des armées du joueur 1
+			System.out.println("joueur " + 2 + " place");
+			j2.initialisation(choix4, j2, 5); // placement des armées du joueur 2
+		}
+		
 }
