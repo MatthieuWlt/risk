e03a54a421a3291a4006a93962e805afa9c891d7
MatthieuWlt <willot.matthieu@gmail.com>
2018-05-30 11:48:47 +0200

    Update Menu.java
    Correction du clic et déplacement des fonctions d'affichage dans une classe View

--- Menu.java
+++ Menu.java
@@ -15,7 +15,7 @@ public class Menu {
 		StdDraw.setCanvasSize(1229, 576);
 		StdDraw.setXscale(0.0, 120);
 		StdDraw.setYscale(0.0, 110);
-		afficherFond(60.0, 55.0, "Image/fond_intro.JPG");
+		View.afficherFond(60.0, 55.0, "Image/fond_intro.JPG");
 
 		// Définition d'une police et de sa taille
 		Font currentFont = StdDraw.getFont();
@@ -36,7 +36,7 @@ public class Menu {
 		
 		while (choix1 != 1) {
 			if (StdDraw.isMousePressed()) {
-				afficherFond(60.0, 55.0, "Image/back2.png");
+				View.afficherFond(60.0, 55.0, "Image/back2.png");
 				choix2 = 0;
 
 				/* Affichage d'une image en fonction du nombre de joueurs */	
@@ -46,23 +46,23 @@ public class Menu {
 						double y = StdDraw.mouseY();
 						StdDraw.clear(StdDraw.WHITE);
 						if (82 < y && y < 99) {
-							afficherImage(60.0, 55.0, "Image/parchemin2.PNG");
+							View.afficherImage(60.0, 55.0, "Image/parchemin2.PNG");
 							choix3 = 2;
 							break;
 						} else if (64 < y && y < 81) {
-							afficherImage(60.0, 55.0, "Image/parchemin3.PNG");
+							View.afficherImage(60.0, 55.0, "Image/parchemin3.PNG");
 							choix3 = 3;
 							break;
 						} else if (46 < y && y < 63) {
-							afficherImage(60.0, 55.0, "Image/parchemin4.PNG");
+							View.afficherImage(60.0, 55.0, "Image/parchemin4.PNG");
 							choix3 = 4;
 							break;
 						} else if (28 < y && y < 45) {
-							afficherImage(60.0, 55.0, "Image/parchemin5.PNG");
+							View.afficherImage(60.0, 55.0, "Image/parchemin5.PNG");
 							choix3 = 5;
 							break;
 						} else if (10 < y && y < 27) {
-							afficherImage(60.0, 55.0, "Image/parchemin6.PNG");
+							View.afficherImage(60.0, 55.0, "Image/parchemin6.PNG");
 							choix3 = 6;
 							break;
 						}
@@ -85,7 +85,7 @@ public class Menu {
 					StdDraw.picture(35.0, 55.0, "Image/Napo1.JPG");
 					StdDraw.text(35.0, 20, "Napoléon");
 					StdDraw.text(85.0, 20, "Jupiter");
-					afficherImage(85.0, 55.0, "Image/Macron.png");
+					View.afficherImage(85.0, 55.0, "Image/Macron.png");
 					choix4 = 0;
 				}
 				
@@ -102,7 +102,7 @@ public class Menu {
 							StdDraw.setPenColor(StdDraw.BLACK);
 							StdDraw.text(60, 90, "Joueur 2, choisissez votre personnage");
 							StdDraw.text(60.0, 20, "Jupiter");
-							afficherImage(60.0, 55.0, "Image/Macron.png");
+							View.afficherImage(60.0, 55.0, "Image/Macron.png");
 							choix5 = 2;
 							break;
 						} else if (55 < x && x < 105) {
@@ -112,7 +112,7 @@ public class Menu {
 							StdDraw.setPenColor(StdDraw.BLACK);
 							StdDraw.text(60, 90, "Joueur 2, choisissez votre personnage");
 							StdDraw.text(60.0, 20, "Napoléon");
-							afficherImage(60.0, 55.0, "Image/Napo1.JPG");
+							View.afficherImage(60.0, 55.0, "Image/Napo1.JPG");
 							choix5 = 2;
 							System.out.println(choix5);
 							break;
@@ -131,21 +131,21 @@ public class Menu {
 					int armee = 40;
 					int choix41 = 1;
 					Partie p = new Partie(42, choix3); // création de la carte
-					p.afficher_map(); // affichage de la carte
+					View.afficher_map(); // affichage de la carte
 					// création joueurs
 					Joueur j1 = new Joueur(0, 1, StdDraw.BLUE, "bleu", null);
 					Joueur j2 = new Joueur(0, 2, StdDraw.GREEN, "vert", null);
 					
 
 					// attribution de la mission au joueur 1
-					affichage_mission(j1);
+					mission.affichage_mission(j1);
 					StdDraw.clear();// on supprime le message de mission
-					p.afficher_map(); // on ré-affiche la carte
+					View.afficher_map(); // on ré-affiche la carte
 
 					// attribution de la mission au joueur 2
-					affichage_mission(j2);
+					mission.affichage_mission(j2);
 					StdDraw.clear();// on supprime le message de mission
-					p.afficher_map(); // on ré-affiche la carte
+					View.afficher_map(); // on ré-affiche la carte
 
 					// liste aléatoire des territoires
 					ArrayList l = new ArrayList<territoire>();
@@ -164,8 +164,8 @@ public class Menu {
 
 					j2.affiche();
 
-					placementArmeeRestantes(j1, choix41);
-					placementArmeeRestantes(j2, choix41);
+					View.placementArmeeRestantes(j1, choix41);
+					View.placementArmeeRestantes(j2, choix41);
 
 					for (int j = 0; j < 50; j++) { // boucle infinie
 						System.out.println("joueur" + 1 + " place");
@@ -199,26 +199,26 @@ public class Menu {
 					int choix41=1;
 					
 					Partie p = new Partie(42, choix3); // création de la carte
-					p.afficher_map(); // affichage de la carte
+					View.afficher_map(); // affichage de la carte
 					// création joueurs
 					Joueur j1 = new Joueur(0, 1, StdDraw.BLUE, "bleu", null);
 					Joueur j2 = new Joueur(0, 2, StdDraw.GREEN, "vert", null);
 					Joueur j3 = new Joueur(0, 3, StdDraw.RED, "rouge", null);
 					
 					// attribution de la mission au joueur 1
-					affichage_mission(j1);
+					mission.affichage_mission(j1);
 					StdDraw.clear();// on supprime le message de mission
-					p.afficher_map(); // on ré-affiche la carte
+					View.afficher_map(); // on ré-affiche la carte
 
 					// attribution de la mission au joueur 2
-					affichage_mission(j2);
+					mission.affichage_mission(j2);
 					StdDraw.clear();// on supprime le message de mission
-					p.afficher_map(); // on ré-affiche la carte
+					View.afficher_map(); // on ré-affiche la carte
 					
 					// attribution de la mission au joueur 3
-					affichage_mission(j3);
+					mission.affichage_mission(j3);
 					StdDraw.clear();// on supprime le message de mission
-					p.afficher_map(); // on ré-affiche la carte
+					View.afficher_map(); // on ré-affiche la carte
 					
 					// liste aléatoire des territoires
 					ArrayList l = new ArrayList<territoire>();
@@ -240,9 +240,9 @@ public class Menu {
 					j2.affiche();
 					j3.affiche();
 					
-					placementArmeeRestantes(j1,choix41);
-					placementArmeeRestantes(j2,choix41);
-					placementArmeeRestantes(j3,choix41);
+					View.placementArmeeRestantes(j1,choix41);
+					View.placementArmeeRestantes(j2,choix41);
+					View.placementArmeeRestantes(j3,choix41);
 
 					while(choix41==1) { // bouocle infinie
 						System.out.println("joueur" + 1 + " place");
@@ -285,7 +285,7 @@ public class Menu {
 					if (StdDraw.mousePressed()) {
 						int armee = 30;
 						Partie p = new Partie(42, choix3); // création de la carte
-						p.afficher_map(); // affichage de la carte
+						View.afficher_map(); // affichage de la carte
 						// création joueurs
 						Joueur j1 = new Joueur(0, 1, StdDraw.BLUE, "bleu", null);
 						Joueur j2 = new Joueur(0, 2, StdDraw.GREEN, "vert", null);
@@ -293,24 +293,24 @@ public class Menu {
 						Joueur j4 = new Joueur(0, 4, StdDraw.ORANGE, "orange", null);
 						
 						// attribution de la mission au joueur 1
-						affichage_mission(j1);
+						mission.affichage_mission(j1);
 						StdDraw.clear();// on supprime le message de mission
-						p.afficher_map(); // on ré-affiche la carte
+						View.afficher_map(); // on ré-affiche la carte
 						
 						// attribution de la mission au joueur 2
-						affichage_mission(j2);
+						mission.affichage_mission(j2);
 						StdDraw.clear();// on supprime le message de mission
-						p.afficher_map(); // on ré-affiche la carte
+						View.afficher_map(); // on ré-affiche la carte
 						
 						// attribution de la mission au joueur 3
-						affichage_mission(j3);
+						mission.affichage_mission(j3);
 						StdDraw.clear();// on supprime le message de mission
-						p.afficher_map(); // on ré-affiche la carte
+						View.afficher_map(); // on ré-affiche la carte
 						
 						// attribution de la mission au joueur 4
-						affichage_mission(j4);
+						mission.affichage_mission(j4);
 						StdDraw.clear();// on supprime le message de mission
-						p.afficher_map(); // on ré-affiche la carte
+						View.afficher_map(); // on ré-affiche la carte
 						
 						// liste aléatoire des territoires
 						ArrayList l = new ArrayList<Integer>();
@@ -336,10 +336,11 @@ public class Menu {
 						j3.affiche();
 						j4.affiche();
 						
-						placementArmeeRestantes(j1,choix41);
-						placementArmeeRestantes(j2,choix41);
-						placementArmeeRestantes(j3,choix41);
-						placementArmeeRestantes(j4,choix41);
+						View.placementArmeeRestantes(j1,choix41);
+						View.placementArmeeRestantes(j2,choix41);
+						View.placementArmeeRestantes(j3,choix41);
+						View.placementArmeeRestantes(j4,choix41);
+						
 						while(choix41==1) { //boucle infinie
 							System.out.println("joueur" + 1 + " place");
 							int r1=j1.verif_region(j1);
@@ -381,50 +382,6 @@ public class Menu {
 				}
 			}
 		}
-		}
 	}
-
-	// Fonction permettant d'afficher une image en fond
-	public static void afficherFond(double x, double y, String fichier) {
-		StdDraw.clear(StdDraw.WHITE);
-		StdDraw.picture(x, y, fichier);
-		StdDraw.pause(500);
-	}
-
-	// Fonction permettant d'afficher une image
-	public static void afficherImage(double x, double y, String fichier) {
-		StdDraw.picture(x, y, fichier);
-		StdDraw.pause(500);
-	}
-	
-	// Fonction permettant d'afficher le nombre d'armées qu'il reste à placer en console
-	public static void placementArmeeRestantes(Joueur j1, int choix4) {
-		System.out.println("Joueur " + j1.getNumero() + " place");
-		j1.initialisation(choix4, j1, 5); // placement des armées du joueur 1
-	}
-	
-	public static void affichage_mission(Joueur j) {
-		int choixmissionj = 0;
-		Random rand = new Random();
-		while (choixmissionj == 0) {
-			if (StdDraw.isMousePressed()) {
-				double x4 = StdDraw.mouseX();// on recupere la coordonnée de x cliqué
-				double y4 = StdDraw.mouseY();// on recupere la coordonnée de y cliqué
-				if (x4 < 116 && x4 > 104 && y4 < 104 && y4 > 96) {// si elles sont sur le bouton
-					int missionj = rand.nextInt(8 - 1) + 1;// on choisit une mission aléatoire
-					j.attribution_mission(missionj, 2, j);// on attribut une mission au joueur 1
-					j.setMission(missionj);// on attribut la mission 1 au joueur 1
-					StdDraw.pause(500);
-					choixmissionj = 1;// on termine la boucle while					
-				} else {
-					choixmissionj = 0;// on redemande au joueur de choisir sa mission
-					StdDraw.text(60, 105, " Joueur "+ j.getNumero()+", découvrez votre mission");// affichage
-				}
-			} else {
-				choixmissionj = 0;// on demande au joueur de choisir sa mission
-				StdDraw.text(60, 105, "Joueur "+ j.getNumero()+", découvrez votre mission");// affichage
-			}
-		}
-	}
-		
+	}		
 }
