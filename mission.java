61b4ddc2d656a9c701e320137e7d31aa43ab1f5d
lionelfayolle <33628543+lionelfayolle@users.noreply.github.com>
2018-06-07 14:47:34 +0200

    Add files via upload

--- mission.java
+++ mission.java
@@ -12,28 +12,71 @@ public class mission {
 		this.numero_mission = numero_mission;
 	}
 	
-	public static void Chat_Box_Mission(Joueur j) {
+	public static void Chat_Box_Mission(Joueur j, Partie p) {
 		int choixmissionj = 0;
+		int missionj=0;
 		Random rand = new Random();
 		while (choixmissionj == 0) {
 			if (StdDraw.isMousePressed()) {
-				double x4 = StdDraw.mouseX();// on recupere la coordonnÃ©e de x cliquÃ©
-				double y4 = StdDraw.mouseY();// on recupere la coordonnÃ©e de y cliquÃ©
+				double x4 = StdDraw.mouseX();// on recupere la coordonnée de x cliqué
+				double y4 = StdDraw.mouseY();// on recupere la coordonnée de y cliqué
 				if (x4 < 116 && x4 > 104 && y4 < 104 && y4 > 96) {// si elles sont sur le bouton
-					int missionj = rand.nextInt(8 - 1) + 1;// on choisit une mission alÃ©atoire
+					if(p.nbre_joueur==2) {
+						int a = rand.nextInt(3 - 1) + 1;
+						if (a==1) {
+							missionj = (rand.nextInt(3 - 1) + 1);// on choisit une mission aléatoire
+							System.out.println(missionj);
+						}
+						else {
+							missionj = (rand.nextInt(9 - 7) + 7);// on choisit une mission aléatoire
+							System.out.println(missionj);
+						}
+					}else if(p.nbre_joueur==3) {
+						int a = rand.nextInt(3 - 1) + 1;
+						if (a==1) {
+							missionj = (rand.nextInt(5 - 1) + 1);// on choisit une mission aléatoire
+							System.out.println(missionj);
+						}
+						else {
+							missionj = (rand.nextInt(9 - 7) + 7);// on choisit une mission aléatoire
+							System.out.println(missionj);
+						}
+					}else if(p.nbre_joueur==4||p.nbre_joueur==5) {
+						int a = rand.nextInt(3 - 1) + 1;
+						if (a==1) {
+							missionj = (rand.nextInt(6 - 3) + 3);// on choisit une mission aléatoire
+							System.out.println(missionj);
+						}
+						else {
+							missionj = (rand.nextInt(9 - 7) + 7);// on choisit une mission aléatoire
+							System.out.println(missionj);
+						}
+					}
+					else if (p.nbre_joueur==6) {
+						int a = rand.nextInt(3 - 1) + 1;
+						if (a==1) {
+							missionj = rand.nextInt(5 - 3) + 3;// on choisit une mission aléatoire
+							System.out.println(missionj);
+						}
+						else {
+							missionj = rand.nextInt(9 - 6) + 6;// on choisit une mission aléatoire
+							System.out.println(missionj);
+						}		
+					}
 					j.attribution_mission(missionj, 2, j);// on attribut une mission au joueur 1
 					j.setMission(missionj);// on attribut la mission 1 au joueur 1
 					StdDraw.pause(500);
 					choixmissionj = 1;// on termine la boucle while					
 				} else {
 					choixmissionj = 0;// on redemande au joueur de choisir sa mission
-					StdDraw.text(60, 105, " Joueur "+ j.getNumero()+", dÃ©couvrez votre mission");// affichage
+					StdDraw.text(60, 105, " Joueur "+ j.getNumero()+", découvrez votre mission");// affichage
 				}
 			} else {
 				choixmissionj = 0;// on demande au joueur de choisir sa mission
-				StdDraw.text(60, 105, "Joueur "+ j.getNumero()+", dÃ©couvrez votre mission");// affichage
+				StdDraw.text(60, 105, "Joueur "+ j.getNumero()+", découvrez votre mission");// affichage
 			}
 		}
 	}
 	
 }
+
