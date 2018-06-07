8e932689294bda36324b0ed27038287b527ab06c
hugodsdt <34475013+hugodsdt@users.noreply.github.com>
2018-06-07 14:01:34 +0200

    Update View.java

--- View.java
+++ View.java
@@ -14,11 +14,11 @@ public class View {
 		StdDraw.picture(x, y, fichier);
 	}
 
-	//Fonction permettant d'afficher le plateau de jeu
+	// Fonction permettant d'afficher le plateau de jeu
 	public static void afficher_map() {
 		StdDraw.clear(StdDraw.WHITE);
-		StdDraw.picture(50.0,50.0,"IMAGE/parchemin_2.png");
-		StdDraw.picture(50.0,50.0,"IMAGE/map.png");
+		StdDraw.picture(50.0, 50.0, "IMAGE/parchemin_2.png");
+		StdDraw.picture(50.0, 50.0, "IMAGE/map.png");
 		StdDraw.setPenColor(StdDraw.GRAY);
 		StdDraw.filledRectangle(110, 100, 8, 4);// bouton mission
 		StdDraw.filledRectangle(110, 90, 8, 4);// bouton finir son tour
@@ -27,39 +27,40 @@ public class View {
 		StdDraw.setPenColor(StdDraw.ORANGE);
 		StdDraw.filledRectangle(50, 105, 35, 5);// chat-box
 		StdDraw.setPenColor(StdDraw.BLACK);// couleur de l'écriture
-		StdDraw.text(110, 100, "mission");// texte
-		StdDraw.text(110, 90, "finir");// texte
-		StdDraw.text(110, 80, "attaque");// texte
-		StdDraw.text(110, 70, "mouvement");// texte
-		StdDraw.text(25, 105, "Chat-Box : ");//texte	
+		StdDraw.text(110, 100, "Mission");// texte
+		StdDraw.text(110, 90, "Finir");// texte
+		StdDraw.text(110, 80, "Attaque");// texte
+		StdDraw.text(110, 70, "Mouvement");// texte
+		StdDraw.text(25, 105, "Chat-Box : ");// text
+
 	}
-	
-	// Fonction qui affiche les images des personnages au moment du choix
+
 	public static void affichage_personnage(int i, String perso1, String perso2, String perso3) {
-		if(i==1) {
+		if (i == 1) {
 			StdDraw.clear(StdDraw.WHITE);
-			StdDraw.picture(50.0,50.0,"IMAGE/parchemin_2.png");
+			StdDraw.picture(50.0, 50.0, "IMAGE/parchemin_2.png");
 			View.afficherImage(60.0, 45.0, perso1);
 			StdDraw.pause(500);
 		}
-		
-		else if (i==2) {
-		StdDraw.clear(StdDraw.WHITE);
-		StdDraw.setPenColor(StdDraw.BLACK);
-		StdDraw.picture(50.0,50.0,"IMAGE/parchemin_2.png");
-		View.afficherImage(35.0, 45.0, perso1);
-		View.afficherImage(85.0, 45.0, perso2);
-		StdDraw.pause(500);
-		}
-		
-		else if (i==3) {
+
+		else if (i == 2) {
 			StdDraw.clear(StdDraw.WHITE);
 			StdDraw.setPenColor(StdDraw.BLACK);
-			StdDraw.picture(50.0,50.0,"IMAGE/parchemin_2.png");
+			StdDraw.picture(50.0, 50.0, "IMAGE/parchemin_2.png");
+			View.afficherImage(35.0, 45.0, perso1);
+			View.afficherImage(85.0, 45.0, perso2);
+			StdDraw.pause(500);
+		}
+
+		else if (i == 3) {
+			StdDraw.clear(StdDraw.WHITE);
+			StdDraw.setPenColor(StdDraw.BLACK);
+			StdDraw.picture(50.0, 50.0, "IMAGE/parchemin_2.png");
 			View.afficherImage(20.0, 45.0, perso1);
 			View.afficherImage(60.0, 45.0, perso2);
 			View.afficherImage(100.0, 45.0, perso3);
 			StdDraw.pause(500);
 		}
 	}
+	
 }
